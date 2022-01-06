60410e1cdc2f80c8db9cef5d70a2a0e886370e24
Fabi0o <marfab32@gmail.com>
2022-01-06 13:24:10 +0100

    created simple ui, and had to change most of the logic for it to work with ui

--- script.js
+++ script.js
@@ -1,54 +1,98 @@
-let rockPaperScissors = ["rock", "paper", "scissors"];
+let rockPaperScissors = ["Rock", "Paper", "Scissors"];
 // randomly picks number from 0-3 (excluding 3), next rounds it down and lastly picks and returns responding move from the array
 function computerPlay() {
-  let computerSelection =
+  const computerSelection =
     rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
   return computerSelection;
 }
 // plays one round and returns and log the result
-function oneRound() {
-  let playerSelection = prompt("Choose: Rock, Paper or sissors:").toLowerCase();
-  let computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
   if (playerSelection == computerSelection) {
-    console.log("It's a draw!");
-    return "draw";
+    return "It's a draw!";
   } else if (
-    (playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "rock")
+    (playerSelection == "Rock" && computerSelection == "Paper") ||
+    (playerSelection == "Paper" && computerSelection == "Scissors") ||
+    (playerSelection == "Scissors" && computerSelection == "Rock")
   ) {
-    playerSelection =
-      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    console.log(`You losse! ${playerSelection} loose to ${computerSelection}.`);
-    return "player lost";
+    return `You lose!`;
   } else if (
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "scissors")
+    (playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper") ||
+    (playerSelection == "Rock" && computerSelection == "Scissors")
   ) {
-    playerSelection =
-      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-    return "player won";
-  } else alert("You have to choose rock, paper or scissors!!!");
-}
-// loops the functions 5 times and saves results
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i <= 4; i++) {
-    let result = oneRound();
-    if (result == "draw") {
-    } else if (result == "player lost") {
-      computerScore++;
-    } else if (result == "player won") {
-      playerScore++;
-    }
+    return `You win!`;
   }
-  if (playerScore > computerScore) {
-    console.log(`You won ${playerScore}:${computerScore}!`);
-  } else if (playerScore < computerScore) {
-    console.log(`You lost ${computerScore}:${playerScore}`);
-  } else console.log(`It's a draw ${computerScore}:${playerScore}`);
 }
-console.log(game());
+const computerSelection = computerPlay();
+const buttons = document.querySelectorAll(".buttons");
+const result = document.querySelector(".results");
+let playerScore = 0;
+let computerScore = 0;
+
+buttons[0].addEventListener("click", function (e) {
+  let gameResult = playRound("Rock", computerSelection);
+  if (gameResult == `You win!`) {
+    playerScore++;
+    result.textContent = `You win! Rock beats scissors! ${playerScore} : ${computerScore}`;
+  } else if (gameResult == `You lose!`) {
+    computerScore++;
+    result.textContent = `You lose! Rock lose to paper! ${playerScore} : ${computerScore}`;
+  } else result.textContent = `It's a draw! ${playerScore} : ${computerScore}`;
+  if (playerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Won ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+  if (computerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Lost ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+});
+buttons[1].addEventListener("click", function (e) {
+  let gameResult = playRound("Paper", computerSelection);
+  if (gameResult == `You win!`) {
+    playerScore++;
+    result.textContent = `You win! Paper beats rock! ${playerScore} : ${computerScore}`;
+  } else if (gameResult == `You lose!`) {
+    computerScore++;
+    result.textContent = `You lose! paper lose to scissors! ${playerScore} : ${computerScore}`;
+  } else result.textContent = `It's a draw! ${playerScore} : ${computerScore}`;
+  if (playerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Won ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+  if (computerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Lost ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+});
+buttons[2].addEventListener("click", function (e) {
+  let gameResult = playRound("Scissors", computerSelection);
+  if (gameResult == `You win!`) {
+    playerScore++;
+    result.textContent = `You win! Scissors beats paper! ${playerScore} : ${computerScore}`;
+  } else if (gameResult == `You lose!`) {
+    computerScore++;
+    result.textContent = `You lose! Scissors lose to rock! ${playerScore} : ${computerScore}`;
+  } else result.textContent = `It's a draw! ${playerScore} : ${computerScore}`;
+  if (playerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Won ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+  if (computerScore >= 5) {
+    buttons[0].remove();
+    buttons[1].remove();
+    buttons[2].remove();
+    result.textContent = `You Lost ${playerScore} : ${computerScore}! If you want to play again, refresh the page.`;
+  }
+});
